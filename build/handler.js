04f4098164d67006375f8b3257670fd915227e6e
Ash Smith <hello@ashsmith.io>
2017-07-24 12:00:29 +0100

    Serverless config for lambda functions

---
+++ build/handler.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var AWS = require('aws-sdk');
+var codebuild = new AWS.CodeBuild();
+var querystring = require('querystring');
+
+
+// this function will be triggered by the github webhook
+module.exports.start_build = (event, context, callback) => {
+  var eventData = querystring.parse(event.body);
+  var githubEvent = JSON.parse(eventData.payload);
+
+  var ref = githubEvent.ref;
+  var sha = githubEvent.after;
+
+  // Deploy any branch to the dev site.
+  var projectName = process.env.BUILD_DEV_PROJECT;
+
+  // Deploy only the master branch to production
+  if (githubEvent.ref.indexOf('/master') !== -1) {
+    projectName = process.env.BUILD_PROJECT;
+  }
+
+  // CodeBuild params
+  var params = {
+    projectName: projectName,
+    sourceVersion: sha
+  };
+
+  // start the codebuild process for this project
+  codebuild.startBuild(params, function(err, data) {
+
+    if (err) {
+      console.log(err, err.stack);
+      callback(err);
+    } else {
+      var build = data.build;
+    }
+  });
+
+  callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ "message": "Sucess!" })
+    });
+
+}
