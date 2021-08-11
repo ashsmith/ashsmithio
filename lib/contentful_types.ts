498da440363f5e33567d14de6e978fb830419612
Ash Smith <hello@ashsmith.io>
2021-08-11 20:42:36 +0000

    lint

--- lib/contentful_types.ts
+++ lib/contentful_types.ts
@@ -1,36 +1,36 @@
 export interface BlogPostFields {
-    title: string;
-    date: string;
-    content: string;
-    category?: string;
-    permalink: string;
-  }
-  
-  export interface GuideStepFields {
-    title: string;
-    content: string;
-  }
-  
-  export interface GuideFields {
-    title: string;
-    slug: string;
-    date: string;
-    introduction?: string;
-    steps: {
-      sys: {
-        id: string;
-      }
-      fields: GuideStepFields
-    }[]
-  }
-  export interface GuideItem {
-    fields: GuideFields;
-  }
-  
-  export interface BlogPostItem {
-    fields: BlogPostFields;
-  }
-  
-  export interface EntriesResponse<T> {
-    items: T[];
-  }
+  title: string;
+  date: string;
+  content: string;
+  category?: string;
+  permalink: string;
+}
+
+export interface GuideStepFields {
+  title: string;
+  content: string;
+}
+
+export interface GuideFields {
+  title: string;
+  slug: string;
+  date: string;
+  introduction?: string;
+  steps: {
+    sys: {
+      id: string;
+    }
+    fields: GuideStepFields
+  }[]
+}
+export interface GuideItem {
+  fields: GuideFields;
+}
+
+export interface BlogPostItem {
+  fields: BlogPostFields;
+}
+
+export interface EntriesResponse<T> {
+  items: T[];
+}
