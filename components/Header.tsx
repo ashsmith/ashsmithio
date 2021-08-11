4293ae86feaa06323d5d1aa870747cd724538916
Ash Smith <hello@ashsmith.io>
2021-08-11 20:30:39 +0000

    h3->h2

--- components/Header.tsx
+++ components/Header.tsx
@@ -51,7 +51,7 @@ const Header: FC = () => (
         <Image src={profilePic} alt="Picture of the author" className="rounded-full" width={95} height={95} placeholder="blur" />
         <div className="flex flex-col items-center ml-8">
           <h1 className="text-5xl font-extrabold">Ash Smith</h1>
-          <h3 className="text-xl">Cloud & Serverless Developer</h3>
+          <h2 className="text-xl">Cloud & Serverless Developer</h2>
         </div>
       </div>
     </div>
