92f523bb052e9274f6d60708f37843d7a41934d4
Ash Smith <hello@ashsmith.io>
2021-08-11 20:22:40 +0000

    Add Head component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Document, {
-  Html, Main, NextScript,
+  Head, Html, Main, NextScript,
 } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <Head />
         <body className="bg-gray-100">
           <Main />
           <NextScript />
