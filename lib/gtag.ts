9d1ee2796775b2bf6c0e85d9c087fb3e8cea21e9
Ash Smith <hello@ashsmith.io>
2021-08-11 20:18:41 +0000

    Remove google analytics, i never use it...

--- lib/gtag.ts
+++
@@ -1,7 +0,0 @@
-export const GA_TRACKING_ID = 'G-XBZHT21955';
-
-export const pageview = (url) => {
-  window.gtag('config', GA_TRACKING_ID, {
-    page_path: url,
-  });
-};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app';
-import { Router } from 'next/router';
 import Layout from '../components/Layout';
-import * as gtag from '../lib/gtag';
 import '../styles/globals.css';
-
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url));
 
 const MyApp:FC<AppProps> = ({ Component, pageProps }) => (
   <Layout>
